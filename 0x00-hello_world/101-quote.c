c3fe62509ac339db094a1ceda0c04a0039ffb3c4
SirVK <victoradesina273@gmail.com>
2022-10-13 16:48:31 -0700

    More on c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - prints a quote
+ * Return:always 0 (success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
