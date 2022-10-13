c4fcc53b1a38bfbfd6398bda3b855e559746c412
SirVK <victoradesina273@gmail.com>
2022-10-13 14:49:40 -0700

    Alligned with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+/**
+* main - prints the phrase "programming is a multilingual puzzle
+* Return: 0, otherwise anything
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
