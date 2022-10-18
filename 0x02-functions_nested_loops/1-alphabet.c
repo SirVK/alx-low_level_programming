44a0ce35f9d4c3b2033d424ebe975714418aa590
SirVK <victoradesina273@gmail.com>
2022-10-18 16:02:23 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphn: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n);
+}
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * add - adds two int
+ * @num1: integer 1
+ * @num2: integer 2
+ * Description: take two ints, returns sum
+ *
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	int sum;
+	sum = numq + num5;
+
+	return (sum);
+}
