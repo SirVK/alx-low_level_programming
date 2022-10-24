b021a64e4da3d4631dd4e26cdf1c08a4a16d60ac
SirVK <victoradesina273@gmail.com>
2022-10-24 08:09:32 -0700

    more patches

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,18 +9,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
-/**
- * main - checks code
- *
- * Return: Always 0
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
-}
