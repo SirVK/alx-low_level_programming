bd7b23d990c1554a1d38dbb18dc982172ff06332
SirVK <victoradesina273@gmail.com>
2022-10-25 02:06:45 -0700

    Starting to think backwards

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s:pointer variable
+ */
+void rev_string(char *s)
+{
+	int r
+		while (s[r] != '\n')
+		{
+			_putchar(s[r]);
+			r--;
+		}
+	_putchar('\n');
+}
